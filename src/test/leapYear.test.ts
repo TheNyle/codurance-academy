77abbf2615eaba635d172dbd38a2d79821dc92ca
Nayo Akinyele <akinyelenayo@gmail.com>
2021-09-03 14:48:43 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/leapYear.test.ts
+++ src/test/leapYear.test.ts
@@ -4,4 +4,7 @@ describe("isLeapYear", () => {
   it("returns false when given 0", () => {
     expect(isLeapYear(0)).toBe(false);
   });
+  it("returns true when give 4", () => {
+    expect(isLeapYear(4)).toBe(true);
+  });
 });
